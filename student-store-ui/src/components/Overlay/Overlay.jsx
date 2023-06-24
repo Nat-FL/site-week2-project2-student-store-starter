e1a76c0d4bee57e3de50a2d8a1946d0156e2071e
Nat-FL <109194818+Nat-FL@users.noreply.github.com>
2023-06-23 23:26:43 -0700

    Add more props to pass

--- student-store-ui/src/components/Overlay/Overlay.jsx
+++ student-store-ui/src/components/Overlay/Overlay.jsx
@@ -3,14 +3,27 @@ import Navbar from "../Navbar/Navbar";
 import Hero from "../Hero/Hero";
 import SearchBar from "../SearchBar/SearchBar";
 import { Outlet } from "react-router-dom";
-
-export default function Overlay({category, setCategory, searchInput, setSearchInput}){
+import Sidebar from "../Sidebar/Sidebar";
+import ShoppingCart from "../ShoppingCart/ShoppingCart";
+export default function Overlay({category, setCategory, searchInput, setSearchInput, shoppingCart, quantity, products, setShoppingCart,totalPrice}){
+    //item={item}
     return(
         <main>
+        <Sidebar 
+        shoppingCart={shoppingCart}
+        quantity={quantity} 
+        products = {products}
+        setShoppingCart = {setShoppingCart}
+        totalPrice = {totalPrice}
+         />
         <Navbar />
         <Hero />
-        <SearchBar searchTerm={searchInput} setSearchTerm={setSearchInput} category={category} setCategory={setCategory} />
-        {/* <About/> */}
+        <SearchBar 
+        searchTerm={searchInput} 
+        setSearchTerm={setSearchInput} 
+        category={category} 
+        setCategory={setCategory} 
+        />
         <Outlet />
 
         </main>
