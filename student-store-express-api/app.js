0a8fd47430ffc3faecd5336b9b12b3be5c4ef400
Nat-FL <109194818+Nat-FL@users.noreply.github.com>
2023-06-23 22:29:03 -0700

    Setting up middleware and routes

--- student-store-express-api/app.js
+++ student-store-express-api/app.js
@@ -1,1 +1,20 @@
-// YOUR CODE HERE
+const express = require("express")
+const cors = require("cors")
+const morgan = require("morgan")
+const app = express()
+const storeRouter = require("./routes/store") //imports storeRouter which can be used to link to store.js(routes) 
+app.use(morgan("dev"))
+app.use(cors())
+app.use(express.json())
+
+//route for store
+app.use("/store", storeRouter) //links front end and back end together-use this to create bridge between desired route that user puts in and response that should occur when route initiated
+
+app.get("/", async (req, res)=>{
+    res.send({ping:"pong"})
+})
+
+
+module.exports = app; 
+
+
