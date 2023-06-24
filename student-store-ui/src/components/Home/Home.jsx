62278cd70c9eced3fe97f53e5d0801fee43a610c
Nat-FL <109194818+Nat-FL@users.noreply.github.com>
2023-06-23 23:25:54 -0700

    Adding/Passing more props to some of the components in Home

--- student-store-ui/src/components/Home/Home.jsx
+++ student-store-ui/src/components/Home/Home.jsx
@@ -5,7 +5,7 @@ import ContactUs from "../Contact/Contact"
 import Footer from "../Footer/Footer"
 import Products from "../Products/Products"
 
-export default function Home({products,searchTerm,category}) {
+export default function Home({products,searchTerm,category, addProduct, removeProduct}) {
 
   
   return (
@@ -13,7 +13,7 @@ export default function Home({products,searchTerm,category}) {
       <div className="products">
         <div className="content">
           <h3>Best Selling Products</h3>
-          <Products products={products} searchTerm={searchTerm} category={category} />
+          <Products products={products} searchTerm={searchTerm} category={category} addProduct={addProduct} removeProduct={removeProduct}/>
           <About />
           <ContactUs />
           <Footer />
