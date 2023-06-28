16aa896b6a5604ad72178da9f1210f25c6d973c9
Nat-FL <109194818+Nat-FL@users.noreply.github.com>
2023-06-27 19:11:36 -0700

    Added comments to file

--- student-store-ui/src/components/Sidebar/Sidebar.jsx
+++ student-store-ui/src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,4 @@
-//import * as React from "react"
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 import "./Sidebar.css"
 import ShoppingCart from "../ShoppingCart/ShoppingCart"
 
@@ -8,21 +7,19 @@ export default function Sidebar({shoppingCart, quantity, products, setShoppingCa
   //sidebar state initially set to false since want bar to be closed
   const [sideBar, setSideBar] = useState(false)
   const [checkout, setCheckout] = useState(false)
-
+  const [subtotal, setSubTotal] = useState(0)
+  const [taxes, setTaxes] = useState(0)
+  const [total, setTotal] = useState(0)
+  
+  //event handeler for sidebar so when user clicks on arrow button sidebar state changes 
   const handleToggleButton = () =>{
     setSideBar((sideBar)=> !sideBar)
    
   }
-  const handleReceiptBtn = () =>{
-
-    setShoppingCart(shoppingCart = [])
-    setCheckout(true)
-    
-  }
-
 
   return (
     
+    //ternary operator used on sidebar section to generate "open" vs "closed" sidebar function based on sideBar state
     <section className={`sidebar ${sideBar ? "open": "closed"}`}>
       <div className="wrapper">
 
@@ -53,56 +50,24 @@ export default function Sidebar({shoppingCart, quantity, products, setShoppingCa
         </div> 
 
       </div>
-      {sideBar && <ShoppingCart  shoppingCart ={shoppingCart} quantity={quantity} products ={products} totalPrice ={totalPrice}/>}
+      {sideBar && <ShoppingCart  
+      shoppingCart ={shoppingCart} 
+      quantity={quantity} 
+      products ={products} 
+      totalPrice ={totalPrice}
+      subtotal = {subtotal}
+      setSubTotal = {setSubTotal}
+      taxes ={taxes}
+      setTaxes = {setTaxes}
+      total = {total}
+      setTotal = {setTotal}
+      setShoppingCart = {setShoppingCart}
+      checkout = {checkout}
+     setCheckout = {setCheckout}
+      />}
 
-      
-    {sideBar && 
-      <div class="checkout-form">
-      <i class="material-icons md-48">monetization_on</i>
-        <h3 class="">Payment Info 
-          <span class="button">
-         
-          </span>
-        </h3>
-        
-        <div class="input-field">
-          <label class="label">Name</label>
-            <div class="control ">
-              <input name="name" class="checkout-form-input" type="text" placeholder="Student Name"></input>
-              </div>
-            </div>
-            
-        <div class="input-field">
-          <label class="label">Email</label>
-            <div class="control"><input name="email" class="checkout-form-input" type="email" placeholder="student@codepath.org"></input>
-            </div>
-        </div>
-
-        <div class="field">
-          <div class="control">
-            <label class="checkbox"><input name="termsAndConditions" type="checkbox"/>
-              <span class="label">I agree to the <a href="#terms-and-conditions">terms and conditions</a>
-              </span>
-            </label>
-          </div>
-        </div>
-        
-        <p class="is-danger"></p>
-        
-        <div class="field">
-          <div class="control">
-            <button class="button checkout-button" onClick={handleReceiptBtn}>Checkout</button>
-            <br/>
-            <br/>
-            <br/>
-            {checkout && <p className="receipt">Success</p>}
-            </div>
-          </div>
-        </div>
-
-      }
-    </section>
-
+   
+    </section>  
     
   );
 }
